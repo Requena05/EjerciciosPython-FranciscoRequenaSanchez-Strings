dab559a39b1c7c56c4c9bec148d712134837b87a
paco <Franreke506@gmail.com>
2024-11-28 16:34:27 +0100

    siuuuu

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import \
     random  # Ejercicio 1 Crea una función que dada una cadena y una letra cuente cuantas veces aparece dicha letra
 def contar_letras(cadena, letra):
-     return cadena.count(letra)
+    if(len(letra)>1):
+       return "Error!"
+    return cadena.count(letra)
 
 # Ejercicio 2. Crea una función que dada una cadena determine si es un palíndromo o
 # si no lo es (sin usar .reversed())
@@ -16,13 +18,13 @@ def palindromo(cadena):
 # • Una con la cadena alrevés (sin usar .reversed())
 def lista_cadenas(cadena):
 
-    return cadena[5:], cadena[:5], cadena[1::2], cadena[0::2], cadena[::-1]
+    return cadena[:5],cadena[-5:], cadena[1::2], cadena[0::2], cadena[::-1]
 
 # Ejercicio 4. Crea una función que dada una cadena determine si es un palíndromo o
 # si no lo es (sin usar .reversed()) y usando slice notation
 
 #El ejercicio 4 ya lo he echo el ejercicio 2 uso esa función para no repetir código
-#Si no esta bien lo siento , suspendeme sino
+
 
 #Ejercicio 5. Crea una función que dada una cadena de texto encuentra la palabra de
 # mayor longitud y la longitud de la misma
@@ -49,17 +51,18 @@ def contar_subcadenas(cadena, subcadena):
 # Ejercicio 8 Crea una función que cuenta cuantas veces aparece una subcadena en un
 # texto y el índice de inicio y fin de cada una de las instancias de la subcadena dentro del texto.
 def contar_subcadenas_y_indices(cadena, subcadena):
-    return cadena.count(subcadena),cadena.index(subcadena),cadena.rindex(subcadena)
+        return cadena.count(subcadena),cadena.index(subcadena),cadena.rindex(subcadena)
 
 
 # Ejercicio 9. Crea una función que encuentre todas las palabras de un texto que
 # empiezan y acaban por una misma subcadena dada
 def encontrar_palabras(cadena:str, subcadena:str):
-    contador=0
+    resultado= []
     for subcaden in cadena.split(" "):
         if subcaden.startswith(subcadena) and subcaden.endswith(subcadena):
-                contador+=1
-    return contador
+                resultado.append(subcaden)
+    return resultado
+
 
 
 # Ejercicio 10. Crea una función que reciba como parámetro un número entero y que
@@ -77,18 +80,16 @@ def tabla_multiplicar(numero):
     print(f"9 * {numero}: ", 9 * numero)
     print(f"10 * {numero}: ", 10 * numero)
 
-
-
 print("Ejercicios de STRINGS")
 print("--------------------------------------------------------")
 print("Ejercicio 1: ")
-print("La cantidad es:",contar_letras("Hola mundo", "o"))
+print("La cantidad es:",contar_letras("Hola mundoooooo", "o"))
 print("--------------------------------------------------------")
 print("Ejercicio 2: ")
 print("Es palindromo:",palindromo("radar"))
 print("--------------------------------------------------------")
 print("Ejercicio 3: ")
-print("Lista de cadenas:",lista_cadenas("Hola mundo"))
+print("Lista de cadenas:",lista_cadenas("Hola mundooooo"))
 print("--------------------------------------------------------")
 print("Ejercicio 4: ")
 print("Es palindromo:",palindromo("radara"))
@@ -100,10 +101,10 @@ print("Ejercicio 6: ")
 print(palabra_mas_larga_mas_corta("Viva e vino de la vidaaaa"))
 print("--------------------------------------------------------")
 print("Ejercicio 7: ")
-print("La cantidad es:",contar_subcadenas("Hola mundo", "o"))
+print("La cantidad es:",contar_subcadenas("Hola mundo", "do"))
 print("--------------------------------------------------------")
 print("Ejercicio 8: ")
-print("La cantidad es:",contar_subcadenas_y_indices("Ta vida es bella", "a"))
+print("La cantidad(1),aparece(2),termina(3):",contar_subcadenas_y_indices("Ta vida es bella", "a"))
 print("--------------------------------------------------------")
 print("Ejercicio 9: ")
 print("La cantidad es:",encontrar_palabras("Ta vidavi es villavi", "vi"))
@@ -156,7 +157,7 @@ def calculaconpipo():
                     print("YES!")
                     acierto = acierto + 1
                 else:
-                    print("Error! ", num_random, "x", tabla_random, "=", respuesta_correcta, "y no", respuesta_usuario)
+                    print("Error! ", num_random, "x", tablas_multi[tabla_random], "=", respuesta_correcta, "y no", respuesta_usuario)
                     falla = falla + 1
 
             print("Has acertado ", acierto, " y has fallado ", falla, " de un total intentos: ", intentos,
